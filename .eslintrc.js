5743a1fe6fd02939a002c21ebe779e87657d085e
peter.slavik <peter.slavik@sprinx.com>
2023-08-23 18:37:23 +0200

    chore: updated eslit cfg

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,11 @@ module.exports = {
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node", 'plugin:typescript-sort-keys/recommended', 'prettier'],
   plugins: ['sort-destructure-keys'],
   rules: {
+    "@typescript-eslint/consistent-type-imports": ["error", {
+      prefer: 'type-imports',
+      disallowTypeAnnotations: true,
+      fixStyle: 'separate-type-imports'
+    }],
     'sort-destructure-keys/sort-destructure-keys': ['error', { caseSensitive: false }],
     'react/jsx-sort-props': [
       'error',
